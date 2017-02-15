e3f49d52d73695e80c1f61f0d227d345d3ecd89e
felixbvf <felixbvf@gmail.com>
2017-02-15 16:32:24 -0400

    Add #npm install -g vue-cli, #vue init webpack-simple front, cd front, # npm install, # npm run dev

---
+++ front/src/App.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class = "container" id="app">
+    <my-navbar></my-navbar>
+  </div>
+</template>
+
+<script>
+import Navbar from './components/Navbar.vue'
+export default {
+    components: {
+        'my-navbar': Navbar
+    }
+}
+</script>
+
+<style lang="scss">
+
+</style>
